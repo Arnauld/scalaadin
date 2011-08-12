0168bd6e5bfe10f1e09e82b653f21d3c615ed720
Arnauld <arnauld.loyer@gmail.com>
2011-08-12 18:10:07 +0200

    spath parser first draft

--- src/test/scala/scalaadin/spath/SPathParserSpecs.scala
+++ src/test/scala/scalaadin/spath/SPathParserSpecs.scala
@@ -1,13 +1,41 @@
 package scalaadin.spath
 
-/**
- * Created by IntelliJ IDEA.
- * User: arnauld
- * Date: 12/08/11
- * Time: 10:32
- * To change this template use File | Settings | File Templates.
- */
+import org.specs.Specification
 
-class SPathParserSpecs {
+class SPathParserSpecs extends Specification {
+  import SPathParser._
 
+  "spath" should {
+    "parse identifier" in {
+      val path = SPathParser("plugins").get
+      path.parts must_== List(AccessorRaw("plugins"))
+    }
+
+    "parse identifier with numerical indices" in {
+      val path = SPathParser("plugins[17]").get
+      path.parts must_== List(AccessorWithIndices("plugins", List(IntLiteral(17))))
+    }
+
+    "parse identifier with literal index" in {
+      val path = SPathParser("""plugins["eav"]""").get
+      path.parts must_== List(AccessorWithIndices("plugins", List(StrLiteral("eav"))))
+    }
+
+    "parse two-sized path with indices" in {
+      val path = SPathParser("plugins.list[3]").get
+      path.parts must_== List(//
+        AccessorRaw("plugins"),//
+        AccessorWithIndices("list",List(IntLiteral(3)))//
+      )
+    }
+
+    "parse complex path with multiple indices" in {
+      val path = SPathParser("plugins.list[3].attributes[1][7]").get
+      path.parts must_== List(//
+        AccessorRaw("plugins"),//
+        AccessorWithIndices("list", List(IntLiteral(3))),//
+        AccessorWithIndices("attributes", List(IntLiteral(1), IntLiteral(7)))//
+      )
+    }
+  }
 }
